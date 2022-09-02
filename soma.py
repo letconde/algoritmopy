8675866b2f0debc3e7222074c4cea45b5492b7fb
Guilherme Dias Ferreira <104311142+GuilhermeDiaF@users.noreply.github.com>
2022-09-02 11:04:22 -0300

    mensagem

---
+++ soma.py
@@ -0,0 +1,24 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: C:/Users/Lab7-Aluno6/soma.py ====================
+Entre com um número: 2
+Entre com um número: 3
+Entre com um número: 4
+Entre com um número: 5
+Entre com um número: 6
+A soma é 20
+/nA media é 4.0
+
+===================== RESTART: C:/Users/Lab7-Aluno6/soma.py ====================
+Entre com um número: 2
+Entre com um número: 4
+Entre com um número: 5
+Entre com um número: 6
+Entre com um número: 7
+A soma é 24
+
+A media é 4.8
+
+===================== RESTART: C:/Users/Lab7-Aluno6/soma.py ====================
+Entre com um número: 
